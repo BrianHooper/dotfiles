50f674e18db562c81451b027d37d7a1cf7843cef
Brian Hooper <brian_hooper@msn.com>
2019-03-13 20:51:13 -0700

    Fix end of month bug in weather script

--- scripts/pyscripts/weather.py
+++ scripts/pyscripts/weather.py
@@ -1,5 +1,5 @@
 from python_wget import http_request
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def print_forecast(city):
@@ -15,13 +15,13 @@ def print_forecast(city):
         exit(1)
 
     current_date = datetime.now()
-    forecast_date = current_date.replace(day=current_date.day + 1, hour=12, minute=0, second=0, microsecond=0)
+    forecast_date = current_date + timedelta(days=1, hours=12)
 
     print(city + ": ")
     for forecast_element in forecast:
         datetime_object = datetime.strptime(forecast_element["dt_txt"], '%Y-%m-%d %H:%M:%S')
         if datetime_object.date() == forecast_date.date():
-            forecast_date = forecast_date.replace(day=forecast_date.day + 1)
+            forecast_date = forecast_date + timedelta(days=1)
             try:
                 temperature = int(forecast_element["main"]["temp"])
                 print("\t" + str(datetime_object.strftime("%A")) + ": " +
